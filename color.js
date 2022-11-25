cff1b2c0d375db1c6882b1a4ea95fe6de898a695
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2022-11-25 22:50:38 +0530

    CB web d paper

---
+++ color.js
@@ -0,0 +1,19 @@
+function ColorChange1() {
+    document.body.style.backgroundColor = "aqua";
+}
+
+function ColorChange2() {
+    document.body.style.backgroundColor = "pink";
+}
+
+function ColorChange3() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function ColorChange4() {
+    document.body.style.backgroundColor = "green";
+}
+
+function ColorChange5() {
+    document.body.style.backgroundColor = "grey";
+}
